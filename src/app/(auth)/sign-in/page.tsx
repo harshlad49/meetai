246fa57163632784c6740a41caa59782dfacb23f
Harsh <harshlad@492002@gmail.com>
2025-07-11 10:58:34 +0530

    04-authui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+
+import { SignInView } from "@/module/auth/ui/views/sign-in-view";
+
+const page = () => {
+  
+  return <SignInView/>
+}
+
+export default page
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+
+
+import { SignUpView } from "@/module/auth/ui/views/sign-up-view";
+
+const page = () => {
+  
+  return <SignUpView/>
+}
+
+export default page
