c80ca99d9858389d6cce0c9fd4d415e85fe2ca63
Harsh <harshlad@492002@gmail.com>
2025-07-17 00:57:29 +0530

    16 meeting setup

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,11 @@
 
 
+import { meetingsRouter } from '@/module/meetings/server/procedures';
 import {  createTRPCRouter } from '../init';
 import { agentsRouter } from '@/module/agents/server/procedures';
 export const appRouter = createTRPCRouter({
   agents: agentsRouter,
+  meetings: meetingsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
